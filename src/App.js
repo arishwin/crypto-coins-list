aa22933920c0a43c50c129d33256fff5b0838a49
Arishwin <arishwinkishaan3@gmail.com>
2022-03-25 15:07:10 +0800

    app functionality working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      coins: [],
+      search: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8000/v1/coins")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ coins: data, filteredCoins: data });
+      });
+  }
+
+  onSearchChange = (e) => {
+    this.setState({
+      search: e.target.value.toLowerCase(),
+    });
+  };
+
+  render() {
+    const { coins, search } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredCoins = coins.filter((coin) => {
+      return coin.name.toLowerCase().includes(search);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search coins"
+          onChange={onSearchChange}
+        />
+        {filteredCoins.map((coin) => {
+          return (
+            <div key={coin.id}>
+              <h1>{coin.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
