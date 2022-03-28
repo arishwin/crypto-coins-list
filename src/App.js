ee1730a7fefd9bde140dfe8e71767d11ffb4a5a6
Arishwin <arishwinkishaan3@gmail.com>
2022-03-28 17:13:29 +0800

    updated App.js to functional component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,76 @@
 import "./App.css";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [search, setSearch] = useState("");
+  const [coins, setCoins] = useState([]);
+  const [filteredCoins, setFilteredCoins] = useState([]);
 
-    this.state = {
-      coins: [],
-      search: "",
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch("http://localhost:8000/v1/coins")
       .then((response) => response.json())
-      .then((data) => {
-        this.setState({ coins: data, filteredCoins: data });
-      });
-  }
+      .then((data) => setCoins(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-  onSearchChange = (e) => {
-    this.setState({
-      search: e.target.value.toLowerCase(),
-    });
+  const onSearchChange = (e) => {
+    setSearch(e.target.value.toLowerCase());
   };
 
-  render() {
-    const { coins, search } = this.state;
-    const { onSearchChange } = this;
-
+  useEffect(() => {
     const filteredCoins = coins.filter((coin) => {
       return coin.name.toLowerCase().includes(search);
     });
+    setFilteredCoins(filteredCoins);
+  }, [search, coins]);
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Crypto Finder</h1>
-        <SearchBox
-          className="search-box"
-          placeholder="search coins"
-          onChangeHandler={onSearchChange}
-        />
-        <CardList coins={filteredCoins} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1 className="app-title">Crypto Finder</h1>
+      <SearchBox
+        className="search-box"
+        placeholder="search coins"
+        onChangeHandler={onSearchChange}
+      />
+      <CardList coins={filteredCoins} />
+    </div>
+  );
+};
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//
+//     this.state = {
+//       coins: [],
+//       search: "",
+//     };
+//   }
+//
+//   componentDidMount() {
+//     fetch("http://localhost:8000/v1/coins")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         this.setState({ coins: data, filteredCoins: data });
+//       });
+//   }
+//
+//   onSearchChange = (e) => {
+//     this.setState({
+//       search: e.target.value.toLowerCase(),
+//     });
+//   };
+//
+//   render() {
+//     const { coins, search } = this.state;
+//     const { onSearchChange } = this;
+//
+
+//
+
+//   }
+// }
 
 export default App;
